49dcb50eb803410a63f877ecc8b50141472c2ab1
LegalPirates2020 <legalpirates2020@gmail.com>
2021-09-10 18:32:55 +0530

    Testing

---
+++ routes/deliveryman-dashboard.route.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const router = express.Router();
+
+const { isDeliveryMan, isLoggedIn } = require('../middleware');
+
+const Orders = require('../models/orders.model');
+
+router
+	.route('/delivery-dashboard')
+	.get(isLoggedIn, isDeliveryMan, async (req, res) => {
+		const assignedOrders = await Orders.find({ deliveredBy: req.user._id })
+			.populate('products')
+			.populate('orderedBy');
+		console.log(assignedOrders);
+		res.render('deliveryman-dashboard', { assignedOrders });
+	})
+	.post(async (req, res) => {
+		const status = req.body.deliveryStatus.split(' ')[0];
+		const orderId = req.body.deliveryStatus.split(' ')[1];
+		const orderStatusUpdate = await Orders.findOneAndUpdate(orderId, {
+			$set: { orderStatus: status }
+		});
+		req.flash('success', 'Delivery status successfull updated.');
+		res.redirect('/delivery-dashboard');
+	});
+
+module.exports = router;
